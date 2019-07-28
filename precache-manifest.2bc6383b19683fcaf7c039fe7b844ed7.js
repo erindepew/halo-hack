e22a194ab31d758ba4cd5d32a6d20c19243a4dcf
Erin Depew <erind@spotify.com>
2019-07-28 19:26:04 -0400

    Updates

--- precache-manifest.2bc6383b19683fcaf7c039fe7b844ed7.js
+++ precache-manifest.2bc6383b19683fcaf7c039fe7b844ed7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1cbca9741cc259363fd22a313663e6d1",
+    "revision": "bf467401f7b0643351629bed0e04b4df",
     "url": "/halo-hack/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/halo-hack/static/js/2.c840b067.chunk.js"
   },
   {
-    "revision": "7d321ceb20c2cec5c803",
-    "url": "/halo-hack/static/js/main.3b46382e.chunk.js"
+    "revision": "52982885520417c2c4a5",
+    "url": "/halo-hack/static/js/main.ac19de09.chunk.js"
   },
   {
     "revision": "b0ddc48688c621453932",
